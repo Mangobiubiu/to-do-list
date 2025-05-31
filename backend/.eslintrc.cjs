a2664932fd44732b63b13a0d4501eb47de0d54b4
Yutong <798704277@qq.com>
2025-05-31 11:00:45 +0200

    Add axios and update new eslint rules

--- backend/.eslintrc.cjs
+++ backend/.eslintrc.cjs
@@ -2,10 +2,20 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: 'eslint:recommended',
+  extends: [
+    'eslint:recommended',
+    'plugin:import/recommended'
+  ],
+  plugins: ['import'],
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  rules: {},
+  rules: {
+    'max-len': ['error', { code: 100 }],
+    'quotes': ['error', 'single'],
+    'no-console': 'warn',
+    'no-var': 'error',
+    'import/no-unresolved': 'error',
+  },
 }
