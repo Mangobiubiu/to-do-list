bb27c2e7aa5901775e0618cf5dc66a5782649479
Yutong <798704277@qq.com>
2025-06-01 19:54:23 +0200

    Add check box in frontend update todos state

--- frontend/src/todos/components/TodoListForm.jsx
+++ frontend/src/todos/components/TodoListForm.jsx
@@ -2,14 +2,11 @@ import React, { useState } from 'react'
 import { TextField, Card, CardContent, CardActions, Button, Typography } from '@mui/material'
 import DeleteIcon from '@mui/icons-material/Delete'
 import AddIcon from '@mui/icons-material/Add'
+import todoActions from '../../constants/TodoActions'
 
 const TodoListForm = ({ todoList, saveTodoList }) => {
   const [todos, setTodos] = useState(todoList.todos)
-
-  const handleSubmit = (event) => {
-    event.preventDefault()
-    saveTodoList(todoList.id, { todos })
-  }
+  const [updatedTodosAction, setUpdatedTodosAction] = useState('')
 
   return (
     <Card sx={{ margin: '0 1rem' }}>
@@ -19,22 +16,34 @@ const TodoListForm = ({ todoList, saveTodoList }) => {
           onSubmit={handleSubmit}
           style={{ display: 'flex', flexDirection: 'column', flexGrow: 1 }}
         >
-          {todos.map((name, index) => (
+          {todos.map(({ text, completed }, index) => (
             <div key={index} style={{ display: 'flex', alignItems: 'center' }}>
+              <Checkbox
+                checked={completed}
+                onChange={(event) => {
+                  setTodos([
+                    ...todos.slice(0, index),
+                    { text: text, completed: event.target.checked },
+                    ...todos.slice(index + 1),
+                  ])
+                  setUpdatedTodosAction(todoActions.UPDATE_COMPLETED)
+                }}
+              />
               <Typography sx={{ margin: '8px' }} variant='h6'>
                 {index + 1}
               </Typography>
               <TextField
                 sx={{ flexGrow: 1, marginTop: '1rem' }}
                 label='What to do?'
-                value={name}
+                value={text}
                 onChange={(event) => {
                   setTodos([
                     // immutable update
                     ...todos.slice(0, index),
-                    event.target.value,
+                    { text: event.target.value, completed: todos[index].completed },
                     ...todos.slice(index + 1),
                   ])
+                  setUpdatedTodosAction(todoActions.UPDATE_TEXT)
                 }}
               />
               <Button
@@ -47,6 +56,7 @@ const TodoListForm = ({ todoList, saveTodoList }) => {
                     ...todos.slice(0, index),
                     ...todos.slice(index + 1),
                   ])
+                  setUpdatedTodosAction(todoActions.DELETE)
                 }}
               >
                 <DeleteIcon />
@@ -58,7 +68,8 @@ const TodoListForm = ({ todoList, saveTodoList }) => {
               type='button'
               color='primary'
               onClick={() => {
-                setTodos([...todos, ''])
+                setTodos([...todos, { text: '', completed: false }])
+                setUpdatedTodosAction(todoActions.CREATE)
               }}
             >
               Add Todo <AddIcon />
